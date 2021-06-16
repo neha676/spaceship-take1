5747ec41210c4dd0ec3b9b7fc3f0bcbbf88ccd2c
Lakshman Choudhary <luckylakshman455@gmail.com>
2021-06-16 12:37:37 +0530

    checker function implemented before post

--- src/app/cube/cube.component.ts
+++ src/app/cube/cube.component.ts
@@ -7,7 +7,7 @@ interface Planet {
   distance: number;
 }
 interface Person {
-  token:string
+  token: string
 }
 // interface Food1 {
 //   value: string;
@@ -17,7 +17,7 @@ interface Person {
 
 const httpOptions = {
   headers: new HttpHeaders({
-    'Content-Type':  'application/json',
+    'Content-Type': 'application/json',
     Authorization: 'my-auth-token'
   })
 };
@@ -28,45 +28,47 @@ const httpOptions = {
   styleUrls: ['./cube.component.css'],
 })
 export class CubeComponent {
-  
+
   vehicles: any[] = [
     {
       "name": "Douglas  Pace",
-      "total_no":34,
-      "max_distance":200,
-      "speed":2
+      "total_no": 34,
+      "max_distance": 200,
+      "speed": 2
     }];
-    showr  = true;
-    showr1 = true;
-    showr2  = true;
-    showr3 = true;
+  showr = true;
+  showr1 = true;
+  showr2 = true;
+  showr3 = true;
   selectedValue = "";
   radioValue = "";
-  radioValue1 ="";
-  selectedValue1 ="";
-  radioValue2 ="";
-  selectedValue2 ="";
-  radioValue3 ="";
-  selectedValue3 ="";
+  radioValue1 = "";
+  selectedValue1 = "";
+  radioValue2 = "";
+  selectedValue2 = "";
+  radioValue3 = "";
+  selectedValue3 = "";
   panelOpenState = false;
-  planet_names = [{name:"hi"}];
-    token ="";
-  buttonGo =true;
+  token = "";
+  buttonGo = true;
   myControl = new FormControl();
   // options: string[] = ['One', 'Two', 'Three'];
-  planets: Planet[] = [{ name: 'steak-0', distance: 34 }];
+  planets: Planet[] = [];
 
 
-    p =['Donlon','Enchai','Jebing','Sapir'];
-      v = ['Space pod','Space Rocket','Space Rocket','Space Rocket']
+  planet_names = ['Donlon', 'Enchai', 'Jebing', 'Sapir'];
+  vehicle_names = ['Space pod', 'Space Rocket', 'Space Rocket', 'Space Rocket']
+
+
+
+
+
   // constructor() { }
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) { }
   ngOnInit(): void {
     this.getPlanet();
     this.getVehicles();
     this.getToken();
-    
-    // this.getToken();
   }
 
   getPlanet() {
@@ -77,100 +79,83 @@ export class CubeComponent {
         this.planets = response;
       });
   }
-  getVehicles(){
+  getVehicles() {
     this.httpClient.get<any>("https://findfalcone.herokuapp.com/vehicles").subscribe(
-      response =>{
+      response => {
         console.log(response);
-        this.vehicles =response;
+        this.vehicles = response;
       }
     )
   }
-  getToken(){
-    const headers = { 'Accept': 'application/json'} 
-    // myHeaders.append("Accept", " application/json ");
+  getToken() {
+    const headers = { 'Accept': 'application/json' }
+
+    this.httpClient.post<any>("https://findfalcone.herokuapp.com/token", {}, { headers }).subscribe(
+      (response) => {
+        this.token = response.token;
+        console.log(this.token)
+      }
+    )
  
-   this.httpClient.post<any>("https://findfalcone.herokuapp.com/token",{},{headers}).subscribe(
-    (response) =>{
-      this.token = response.token;
-      console.log(response.token);
-    console.log(this.token) }
-   )
-    // console.log(h)
-    // console.log("sdzfsdzf ")
   }
 
-  Find(){
-    const headers = { 'Accept': 'application/json','Content-Type': 'application/json'} 
-    // myHeaders.append("Accept", " application/json ");
-    
-   this.httpClient.post<any>("https://findfalcone.herokuapp.com/find",{"token":this.token,"planet_names":this.p,"vehicle_names":this.v},{headers}).subscribe(
-    (response) =>{
-      console.log(response); }
-   )
-    // console.log(h)
-    // console.log("sdzfsdzf ")
+  Find() {
+    const headers = { 'Accept': 'application/json', 'Content-Type': 'application/json' }
+    this.httpClient.post<any>("https://findfalcone.herokuapp.com/find", { "token": this.token, "planet_names": this.planet_names, "vehicle_names": this.vehicle_names }, { headers }).subscribe(
+      (response) => {
+        console.log(response);
+      }
+    )
   }
 
 
-  checker(){
-    if (this.selectedValue !="" &&this.selectedValue1 !="" && this.selectedValue2 !="" && this.selectedValue3 !=""){
-      this.buttonGo =false;
+  checker() {
+    if (this.selectedValue != "" && this.selectedValue1 != "" && this.selectedValue2 != "" && this.selectedValue3 != "" && this.radioValue != "" && this.radioValue1 != "" && this.radioValue2 != "" && this.radioValue3 != ""   ) {
+      
+      this.buttonGo = false;
     }
   }
 
-  // selectedDevice = 'SDsDsdss';
-  // showr= "true"
-  //   onChange(newValue:string ) {
-  //     console.log(newValue);
-  //     this.selectedDevice = newValue;
-  //     console.log(this.selectedDevice)
-  //     // ... do other stuff here ...
-  // }
-  // onChange(value: string) {
-   
-  //   // this.showr=false;
-  
-  // }
-  
-  showRadio(value: string){
+
+  showRadio(value: string) {
     this.checker();
     return this.showr = false;
   }
 
-  showRadio1(value: string){
+  showRadio1(value: string) {
     this.checker();
-    return this.showr1= false;
+    return this.showr1 = false;
   }
-  
-  showRadio2(value: string){
+
+  showRadio2(value: string) {
     this.checker();
     return this.showr2 = false;
   }
-  
-  showRadio3(value: string){
+
+  showRadio3(value: string) {
     this.checker();
     return this.showr3 = false;
   }
-  myClickFunction(){
-    console.log("sdzsd")
-    // this.getToken();
-    // console.log(this.selectedValue)
-    // console.log([this.selectedValue, this.selectedValue1])
-    this.p.pop()
-    this.p.pop()
-    this.p.pop()
-    this.p.pop()
-    this.p.push(this.selectedValue,this.selectedValue1,this.selectedValue2,this.selectedValue3)
-    this.v.pop()
-    this.v.pop()
-    this.v.pop()
-    this.v.pop()
-    this.v.push(this.radioValue,this.radioValue1,this.radioValue2,this.radioValue3)
 
-    console.log(this.v)
-    console.log(this.p)
-        // this.planet_names.push(this.selectedValue);
-  // this.planet_names.push(this.selectedValue);
+
+  showBox(value: string){
+    this.checker();
+  }
+  myClickFunction() {
+    this.planet_names.pop()
+    this.planet_names.pop()
+    this.planet_names.pop()
+    this.planet_names.pop()
+    this.planet_names.push(this.selectedValue, this.selectedValue1, this.selectedValue2, this.selectedValue3)
+    
+    this.vehicle_names.pop()
+    this.vehicle_names.pop()
+    this.vehicle_names.pop()
+    this.vehicle_names.pop()
+    
+    this.vehicle_names.push(this.radioValue, this.radioValue1, this.radioValue2, this.radioValue3)
+    console.log("planet_names:" + this.planet_names)
+    console.log("vehicle_names:" + this.vehicle_names)
     this.Find();
   }
 }
