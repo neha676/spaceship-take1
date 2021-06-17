823a2cc719d19e600edcd0e30084c802e9bbc5da
Lakshman Choudhary <luckylakshman455@gmail.com>
2021-06-17 23:08:52 +0530

    lot og things

--- src/app/cube/cube.component.ts
+++ src/app/cube/cube.component.ts
@@ -36,39 +36,42 @@ export class CubeComponent {
       "max_distance": 200,
       "speed": 2
     }];
-  showr = true;
-  showr1 = true;
-  showr2 = true;
-  showr3 = true;
-  selectedValue = "";
-  radioValue = "";
-  radioValue1 = "";
-  selectedValue1 = "";
-  radioValue2 = "";
-  selectedValue2 = "";
-  radioValue3 = "";
-  selectedValue3 = "";
+
+
+  // selectedPlanets =[];
+
+  
+
   panelOpenState = false;
   token = "";
-  buttonGo = true;
+  buttonGo = false;
   myControl = new FormControl();
   // options: string[] = ['One', 'Two', 'Three'];
   planets: Planet[] = [];
-status ="";
+  status ="";
 
   planet_names = ['Donlon', 'Enchai', 'Jebing', 'Sapir'];
   vehicle_names = ['Space pod', 'Space Rocket', 'Space Rocket', 'Space Rocket']
 
+  selectedPlanets: { [i: number]: string }  = {};
+  selectedVehicles: { [i: number]:  string } = {};
+
+  previousSection=0;
+  previousButton="";
 
 
+  planet_data: {[i:string ]:number} ={};
 
-
+  
+  vehicle_data: {[i:string ]:number} ={};
+  vehicle_speed: {[i:string ]:number} ={}
   // constructor() { }
   constructor(private httpClient: HttpClient) { }
   ngOnInit(): void {
     this.getPlanet();
     this.getVehicles();
     this.getToken();
+
   }
 
   getPlanet() {
@@ -77,6 +80,10 @@ status ="";
       .subscribe((response) => {
         console.log(response);
         this.planets = response;
+        for (let i = 0; i <response.length; i++) {
+          this.planet_data[response[i].name]=response[i].distance;
+        }
+        console.log(this.planet_data);
       });
   }
   getVehicles() {
@@ -84,6 +91,12 @@ status ="";
       response => {
         console.log(response);
         this.vehicles = response;
+        for (let i = 0; i <response.length; i++) {
+          this.vehicle_data[response[i].name]=response[i].max_distance;
+          this.vehicle_speed[response[i].name]=response[i].speed;
+          
+        }
+        console.log(this.vehicle_data);
       }
     )
   }
@@ -109,52 +122,115 @@ status ="";
     )
   }
 
+    showpod( current_planet_name :string, current_vehicle_name:string) {
 
-  checker() {
-    if (this.selectedValue != "" && this.selectedValue1 != "" && this.selectedValue2 != "" && this.selectedValue3 != "" && this.radioValue != "" && this.radioValue1 != "" && this.radioValue2 != "" && this.radioValue3 != ""   ) {
+    if(!current_planet_name)
+    return true
+    let currentvehicledistance = this.vehicle_data[current_vehicle_name]
+    // console.log(current_vehicle_name)
+    // console.log(currentvehicledistance)
+    // console.log(current_planet_name)
+    // console.log( this.planet_data[current_planet_name])
+    
+    if (currentvehicledistance < this.planet_data[current_planet_name])
+   return true;
+    
+    return false;
+  }
+  // checker(value: string) {
+  //   console.log(value);
+    
+  //   for (let vehicle of  this.vehicles){
+  //     if(vehicle.name == value){
+  //       vehicle.total_no = vehicle.total_no - 1;
+  //     }
+  //   }
+
+
+  //   if (this.selectedValue != "" && this.selectedValue1 != "" && this.selectedValue2 != "" && this.selectedValue3 != "" && this.radioValue != "" && this.radioValue1 != "" && this.radioValue2 != "" && this.radioValue3 != ""   ) {
       
-      this.buttonGo = false;
+  //     this.buttonGo = false;
+  //   }
+  // }
+  check(value: any, i :number){
+
+    this.buttonGo =true;
+    // console.log(this.previousButton);
+      // this.previousButton = this.selectedVehicles[i];
+      this.selectedVehicles[i] = value;
+for ( i =1 ; i <=4 ; i++ )
+{
+  // console.log('checkin',i)
+  if ( !(i in this.selectedVehicles)){
+    this.buttonGo = false;
+    break;
+
+  }
+}   
+      
+
+  }
+  openGroup(value: number){
+    // this.previousButton = "";
+    if(this.previousSection != 0){
+      //calcualte
+      let veh_to_remo= this.selectedVehicles[this.previousSection]
+      let veh_to_add= this.selectedVehicles[value]
+     
+
+      for (let i=0;i<this.vehicles.length;i++)
+      {   
+          if(this.vehicles[i].name===veh_to_remo){this.vehicles[i].total_no--}
+          if(veh_to_add && this.vehicles[i].name===veh_to_add){this.vehicles[i].total_no++}
+
+      }
+
     }
+    this.previousSection=value;
   }
 
 
-  showRadio(value: string) {
-    this.checker();
-    return this.showr = false;
+  showplanets( planet :string){
+// console.log(this.selectedPlanets)
+let show = true ; 
+// console.log("checking planet:",planet) 
+    for(let i=0; i<=4;i++){
+      // console.log("checking ",this.selectedPlanets[i])
+      if ( this.selectedPlanets[i] && planet ==this.selectedPlanets[i]){
+        // console.log("setting to false")
+        return false;
+      }
+      
+      // console.log(this.selectedPlanets[i])
+    }
+    return true;
+    // console.log(Object.keys(this.selectedPlanets))
+    // if(planet in  Object.keys(this.selectedPlanets).map(key => this.selectedPlanets[key])){
+    //   return true;
+    // }
+    // else return false;
   }
 
-  showRadio1(value: string) {
-    this.checker();
-    return this.showr1 = false;
-  }
+  showRadio(value: string, i:number) {
+    this.selectedVehicles[i]=""
+    this.buttonGo=false;
 
-  showRadio2(value: string) {
-    this.checker();
-    return this.showr2 = false;
-  }
-
-  showRadio3(value: string) {
-    this.checker();
-    return this.showr3 = false;
+    // this.checker(value);
+    
   }
 
 
   showBox(value: string){
-    this.checker();
+    // this.checker(value);
   }
   myClickFunction() {
     this.planet_names.pop()
     this.planet_names.pop()
     this.planet_names.pop()
     this.planet_names.pop()
-    this.planet_names.push(this.selectedValue, this.selectedValue1, this.selectedValue2, this.selectedValue3)
-    
-    this.vehicle_names.pop()
-    this.vehicle_names.pop()
-    this.vehicle_names.pop()
-    this.vehicle_names.pop()
-    
-    this.vehicle_names.push(this.radioValue, this.radioValue1, this.radioValue2, this.radioValue3)
+    // this.planet_names.push(this.selectedValue, this.selectedValue1, this.selectedValue2, this.selectedValue3)
+    console.log(this.selectedPlanets)
+    // this.vehicle_names.push(this.radioValue, this.radioValue1, this.radioValue2, this.radioValue3)
     console.log("planet_names:" + this.planet_names)
     console.log("vehicle_names:" + this.vehicle_names)
     this.Find();
